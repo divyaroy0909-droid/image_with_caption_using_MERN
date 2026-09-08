a59ed65a943772029f92173cbdbce99d7e9fdeac
Divyanshu Roy <divyaroy0909@gmail.com>
2026-09-09 01:48:02 +0530

    Add files via upload

---
+++ backend/src/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const multer = require('multer');
+
+const uploadFile = require("./services/storage.service")
+
+const postModel = require("./model/post.model")
+const cors = require("cors")
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors())
+
+const upload = multer({ storage : multer.memoryStorage() })
+
+app.post('/create-post', upload.single("image"), async (req, res) => {
+
+    console.log(req.body);
+    console.log(req.file);
+
+    const result = await uploadFile(req.file.buffer);
+
+    console.log(result);
+
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    });
+
+    return res.status(201).json({
+        message: "Post created successfully",
+        post
+    });
+});
+
+app.get('/posts', async (req, res) => {
+
+    const posts = await postModel.find();
+
+    return res.status(200).json({
+        posts
+    });
+});
+
+
+module.exports = app
